9c37a1ddfcb5c09f1b9a076cbd359e88967fe78c
dankato <apearmor@gmail.com>
2017-12-31 15:08:28 -0800

    adding config for communicating to mysql db

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+// turning on output buffering, when a php page loads it will send data to the server in pieces, waiting for all the data before sending it to the server
+ob_start();
+$timezone = date_default_timezone_set('America/Los_Angeles');
+
+// server name, username, password, db name
+$con = mysqli_connect('localhost', 'root', '', 'Cytosine-Flop');
+if(mysqli_connect_errno()) {
+  echo 'Failed to connect: ' . mysqli_connect_errno();
+}
+?>
