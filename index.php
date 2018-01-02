f9df74c440320d6b0c7db3e2fcd25044d8b46cfd
dankato <apearmor@gmail.com>
2018-01-01 22:22:52 -0800

    initial setup for now playing bar

--- index.php
+++ index.php
@@ -16,9 +16,24 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Welcome to Cytosine Flow</title>
+    <link rel="stylesheet" href="assets/css/style.css">
   </head>
   <body>
-    hello there
+
+    <div id="nowPlayingBarContainer">
+      <div id="nowPlayingBar">
+        <div id="nowPlayingLeft">
+
+        </div>
+        <div id="nowPlayingCenter">
+
+        </div>
+        <div id="nowPlayingRight">
+
+        </div>
+      </div>
+    </div>
+
   </body>
 </html>
