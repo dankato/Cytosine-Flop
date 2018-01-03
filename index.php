ad18833ce0a414fbfcd158519dc76ac37f46b8f9
dankato <apearmor@gmail.com>
2018-01-02 19:47:48 -0800

    added music, album covers, and updated main content view

--- index.php
+++ index.php
@@ -25,105 +25,15 @@
 
   <div id="mainContainer">
     <div id="topContainer">
-      <div id="navBarContainer">
-        <nav class="navBar">
-          <a href="index.php" class="logo">
-            <img src="assets/images/icons/logo.png" alt="Logo">
-          </a>
-
-          <div class="group">
-            <div class="navItem">
-              <a href="search.php" class="navItemLink">Search
-                <img src="assets/images/icons/search.png" class="icon" alt="Search">
-              </a>
-            </div>
-          </div>
-          <div class="group">
-            <div class="navItem">
-              <a href="browse.php" class="navItemLink">Browse</a>
-            </div>
-            <div class="navItem">
-              <a href="yourMusic.php" class="navItemLink">Your Music</a>
-            </div>
-            <div class="navItem">
-              <a href="profile.php" class="navItemLink">Profile</a>
-            </div>
-          </div>
-
-        </nav>
-      </div>
-    </div>
-    <div id="nowPlayingBarContainer">
-      <div id="nowPlayingBar">
-
-        <div id="nowPlayingLeft">
-          <div class="content">
-            <span class="albumLink">
-                <img src="https://pbs.twimg.com/profile_images/923950005928452096/iBu5RASh_400x400.jpg" class="albumArtwork" alt="Album Artwork">
-              </span>
-            <div class="trackInfo">
-              <span class="trackName">
-                  <span>Song Name</span>
-              </span>
-              <span class="artistName">
-                  <span>Artist Name</span>
-              </span>
-            </div>
-          </div>
-        </div>
-
-        <div id="nowPlayingCenter">
-          <div class="content playerControls">
-            <div class="buttons">
-              <button class="controlButton shuffle" title="Shuffle button" type="button" name="button">
-                  <img src="assets/images/icons/shuffle.png" alt="Shuffle">
-                </button>
-              <button class="controlButton previous" title="Previous button" type="button" name="button">
-                  <img src="assets/images/icons/previous.png" alt="Previous">
-                </button>
-              <button class="controlButton play" title="Play button" type="button" name="button">
-                  <img src="assets/images/icons/play.png" alt="Play">
-                </button>
-              <button class="controlButton pause" title="Pause button" style="display: none;" type="button" name="button">
-                  <img src="assets/images/icons/pause.png" alt="Pause">
-                </button>
-              <button class="controlButton next" title="Next button" type="button" name="button">
-                  <img src="assets/images/icons/next.png" alt="Next">
-                </button>
-              <button class="controlButton repeat" title="Repeat button" type="button" name="button">
-                  <img src="assets/images/icons/repeat.png" alt="Repeat">
-                </button>
-            </div>
-
-            <div class="playbackBar">
-              <span class="progressTime current">0.00</span>
-              <div class="progressBar">
-                <div class="progressBarBg">
-                  <div class="progress">
-                  </div>
-                </div>
-              </div>
-              <span class="progressTime remaining">0.00</span>
-            </div>
-
-          </div>
-        </div>
-        <div id="nowPlayingRight">
-          <div class="volumeBar">
-            <button type="button" class="controlButton volumn" title="Volume button">
-                <img src="assets/images/icons/volume.png" alt="Volume">
-              </button>
-            <div class="progressBar">
-              <div class="progressBarBg">
-                <div class="progress">
-                </div>
-              </div>
-            </div>
-          </div>
+      <?php include("includes/navbarContainer.php"); ?>
+      <div id="mainViewContainer">
+        <div id="mainContent">
+          
         </div>
       </div>
-    </div>
 
+    </div>
+      <?php include("includes/nowPlayingBar.php"); ?>
   </div>
 </body>
 
