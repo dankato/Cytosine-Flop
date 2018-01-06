631a219234ac9a5633b4eaa91982ca6b17f724e3
dankato <apearmor@gmail.com>
2018-01-05 23:06:57 -0800

    building the search function

--- search.php
+++ search.php
@@ -11,5 +11,22 @@
 
 <div class="searchContainer">
   <h4>Search Cytosine Flop</h4>
-  <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing...">
+  <input type="text" class="searchInput" value="<?php echo $term; ?>" placeholder="Start typing..." onfocus="var val=this.value; this.value=''; this.value= val;"
 </div>
+
+<script>
+
+  $(function() {
+    var timer;
+    $(".searchInput").keyup(function() {
+      // while typing, cancel out existing timer
+      clearTimeout(timer);
+      // after canceled, create a brand new timer of 2 secs
+      timer = setTimeout(function() {
+        var value = $(".searchInput").val();
+        openPage("search.php?term=" + value);
+      }, 1500);
+    })
+    $(".searchInput").focus();
+  })
+</script>
