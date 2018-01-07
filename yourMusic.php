59c2384ef378b0c808343148820ccd1b466b5ad9
dankato <apearmor@gmail.com>
2018-01-06 21:29:03 -0800

    setting up playlist page

---
+++ yourMusic.php
@@ -0,0 +1,12 @@
+<?php
+  include("includes/includedFiles.php");
+?>
+
+<div class="playlistsContainer">
+  <div class="gridViewContainer">
+    <h2>Playlists</h2>
+    <div class="buttonItems">
+      <button class="button green" onClick="#">New Playlist</button>
+    </div>
+  </div>
+</div>
